a4e5832613df2662664fc45f68c8ddf28053f5fb
PabloTerrazas16 <pa.terrazas@duocuc.cl>
2026-07-10 00:08:18 -0400

    alo

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,26 +1,11 @@
-resource "aws_ecr_repository" "api" {
-  name                 = "${var.project_name}-api"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+data "aws_ecr_repository" "api" {
+  name = "${var.project_name}-api"
 }
 
-resource "aws_ecr_repository" "frontend" {
-  name                 = "${var.project_name}-frontend"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+data "aws_ecr_repository" "frontend" {
+  name = "${var.project_name}-frontend"
 }
 
-resource "aws_ecr_repository" "db" {
-  name                 = "${var.project_name}-db"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+data "aws_ecr_repository" "db" {
+  name = "${var.project_name}-db"
 }
